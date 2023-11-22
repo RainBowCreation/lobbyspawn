22601a66cec125f5607f37d3fe4d9a964be229f3
VocanicZ <ampnattahapat@gmail.com>
2023-11-22 11:34:46 +0700

    Make mod compatible with loginer v1.1+

--- src/main/java/net/rainbowcreation/lobbyspawn/event/OnPlayerJoin.java
+++ src/main/java/net/rainbowcreation/lobbyspawn/event/OnPlayerJoin.java
@@ -1,6 +1,7 @@
 package net.rainbowcreation.lobbyspawn.event;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.rainbowcreation.lobbyspawn.Main;
@@ -10,9 +11,16 @@ import static net.rainbowcreation.lobbyspawn.config.GeneralConfig.settings;
 public class OnPlayerJoin {
     @SubscribeEvent
     public static void onJoin(net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent event) {
+        if (Loader.isModLoaded("RBCloginer"))
+            return;
         if (!settings.ENABLE)
             return;
-        EntityPlayer player = event.player;
+        onJoin(event.player);
+    }
+
+    public static void onJoin(EntityPlayer player) {
+        if (!settings.ENABLE)
+            return;
         if (!Main.bypass.contains(player.getName())) {
             player.setPositionAndUpdate(settings.X, settings.Y, settings.Z);
         }
--- src/main/java/net/rainbowcreation/lobbyspawn/utils/Reference.java
+++ src/main/java/net/rainbowcreation/lobbyspawn/utils/Reference.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Reference {
     public static final String MODID = "rbclobbyspawn";
     public static final String NAME = "RBClobbyspawn";
-    public static final String VERSION = "1.12.2-v1";
+    public static final String VERSION = "1.12.2-v1.1";
     public static final List<String> HEADER = Arrays.asList("######################################################################################",
             "#  _____       _       ____                 _____                _   _               #",
             "# |  __ \\     (_)     |  _ \\               / ____|              | | (_)              #",
